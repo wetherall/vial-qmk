74d7eeb032d2fc70c4817ddf6fcd0e887df13405
wetherall <wetherall@users.noreply.github.com>
2021-08-09 10:42:52 +0100

    swapped ctrl and capslock around on vial keymap

--- keyboards/atoll/keymaps/vial/keymap.c
+++ keyboards/atoll/keymaps/vial/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_CAPS,   _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_QUOT, KC_TILD, KC_ENT,
+        KC_CAPS,   _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_QUOT, KC_ENT,
         _______,       KC_BSLS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                      _______,   _______,    _______,            _______,                 _______, _______
     ),
